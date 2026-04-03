ae3e783da888d0a9b808881f7e019fc0a46991f5
jmoran146006tc-svg <j.moran.146006.tc@umindanao.edu.ph>
2026-04-03 21:48:38 +0800

    The Darkness Drops Again

---
+++ resources/views/showcases.blade.php
@@ -0,0 +1,6 @@
+
+<x-layout title="Showcases"><br><br><br><br>
+    <x-card>
+        <p>Check out our latest showcases!</p>
+    </x-card>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,68 @@ Route::view('/', 'welcome', [
     ],
 ]);
 
-Route::view('/about', 'about');
-Route::view('/contact', 'contact');
 
-Route::get('/formtest', function(){
-    $emails = session()->get('$emails', []);
+Route::get('/about', function () {
+    return view('about');
+});
 
-    return view('formtest',[
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/showcases', function () {
+    return view('showcases');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/formtest', function () {
+    $emails = session()->get('emails', []);
+
+    return view('formtest', [
         'emails' => $emails,
     ]);
 });
 
-Route::post('/formtest', function(){
-    $email = request('email');
+Route::post('/formtest', function () {
+    $validated = request()->validate([
+        'email' => 'required|email',
+    ]);
+    $emails = session()->get('emails', []);
 
-    session()->push('$emails', $email);
+    if (in_array($validated['email'], session()->get('emails', []))) {
+        return redirect('/formtest')->withErrors(['email' => 'This email has already been submitted.']);
+    }
 
+    if (count($emails) >= 5) {
+        return redirect('/formtest')->with('warning', 'Maximum of 5 emails reached.');
+    }
+
+    session()->push('emails', $validated['email']);
+    session()->flash('success', 'Email added successfully!');
     return redirect('/formtest');
 });
 
-Route::get('/delete-emails', function(){
-    session()->forget('$emails');
+Route::delete('/formtest', function () {
+    $email = request('email');
+
+    $emails = session()->get('emails', []);
+
+    $emails = array_values(array_filter($emails, fn($e) => $e !== $email));
+
+    session()->put('emails', $emails);
+    session()->flash('success', 'Email deleted.');
+    return redirect('/formtest');
+});
+
+Route::get('/delete-emails', function () {
+    session()->forget('emails');
+
     return redirect('/formtest');
 });
